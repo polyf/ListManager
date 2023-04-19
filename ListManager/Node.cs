27ab09e75ecd2a0e63df017d4080e93985c1e2dc
Polyana Fucilini <93053361+polyf@users.noreply.github.com>
2023-04-18 21:00:22 -0300

    first commit

---
+++ ListManager/Node.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static System.Net.Mime.MediaTypeNames;
+
+namespace ListManager
+{
+    public class Node
+    {
+
+        public int Value { get; set; }
+        public Node? Next { get; set; }
+
+        public Node? Prev { get; set; }
+
+        public Node(int value)
+        {
+            Value = value;
+            Next = null;
+            Prev = null;
+        }
+    }
+}
---
+++ ListManager/NodeList.cs
@@ -0,0 +1,34 @@
+﻿namespace ListManager
+{
+    public class NodeList
+    {
+        public Node? Head { get; set; }
+        public Node? Tail { get; set; }
+        public int Size;
+
+        public NodeList()
+        {
+            Head = null;
+            Tail = null;
+            Size = 0;
+        }
+
+        public NodeList Init ()
+        {
+            NodeList list = new NodeList();
+            return list;
+        }
+        
+        public bool IsListEmpty()
+        {
+            return Head != null && Tail != null;
+        }
+
+
+        public void Add(int value)
+        {
+            
+        }
+
+    }
+}
